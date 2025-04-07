46e0c926ea6040c8b0a2c7e754fb06e85342eb83
Елена Руднева <halley.stearate@gmail.com>
2025-04-07 11:23:17 +0300

    add first commit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+"""Starting programing game."""
 from random import randint
+#from graphic_arts.start_game_banner import run_screensaver
 
 
 def attack(char_name, char_class):
@@ -83,16 +85,17 @@ def choice_char_class():
     return char_class
 
 
-def main():
+if __name__ == '__main__':
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
-    char_name = input('...назови себя: ')
+    char_name: str = input('...назови себя: ')
     print(f'Здравствуй, {char_name}! '
           'Сейчас твоя выносливость — 80, атака — 5 и защита — 10.')
     print('Ты можешь выбрать один из трёх путей силы:')
     print('Воитель, Маг, Лекарь')
-    char_class = choice_char_class()
-    print(start_training(char_name, char_class))
+    char_class: str = choice_char_class()
+    print(start_training(char_name, char_class)) 
 
 
 main()
